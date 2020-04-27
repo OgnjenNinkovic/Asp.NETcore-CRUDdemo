a07bb059083d9aecb8940b64f8e492ef504a2aaa
unknown <oninkovic5@gmail.com>
2020-04-27 22:15:45 +0200

    Asp.net app full commit

---
+++ DemoApp/Controllers/EmployeeController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DemoApp.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace DemoApp.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly EmployeesContext _context;
+
+        public EmployeeController(EmployeesContext context)
+        {
+            _context = context;
+        }
+
+       // GET: Employee
+       [Authorize]
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Employees.ToListAsync());
+        }
+
+        [Authorize]
+        [HttpGet]
+        public async Task<IActionResult> Index(string EmpSearch)
+        {
+            ViewData["Getemployeedetails"] = EmpSearch;
+
+            var q = from e in _context.Employees select e;
+
+            if (!string.IsNullOrEmpty(EmpSearch))
+            {
+               q= q.Where(e => e.FullName.Contains(EmpSearch) || e.EmpCode.Contains(EmpSearch));
+            }
+
+
+
+            return View(await q.AsNoTracking().ToListAsync());
+
+        }
+
+
+        // GET: Employee/Create
+        public IActionResult AddOrEdit(int id=0)
+        {
+            if (id == 0)
+            {
+                return View(new Employee());
+            }
+            else
+            {
+                return View(_context.Employees.Find(id));
+
+            }
+        }
+
+        // POST: Employee/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddOrEdit([Bind("EmployeeId,FullName,EmpCode,Position,OfficeLocation")] Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                if(employee.EmployeeId == 0)
+                {
+                    _context.Add(employee);
+                }
+                else
+                {
+                    _context.Update(employee);
+                }
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(employee);
+        }
+
+      
+        public async Task<IActionResult> Delete(int? id)
+        {
+            var employee = await _context.Employees.FindAsync(id);
+            _context.Employees.Remove(employee);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+      
+        private bool EmployeeExists(int id)
+        {
+            return _context.Employees.Any(e => e.EmployeeId == id);
+        }
+    }
+}
